bcf6b5779bce03a85b0ca4a9f33f970b1e31c8c3
poemaman <luis.campos.pousa@gmail.com>
2024-03-05 19:52:39 +0100

    feat: muestro listado de pokemon

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -9,14 +9,39 @@ class PokemonController extends AbstractController
 {
 
     #[Route("/pokemon")]
-    public function getPokemon(){
-            $pokemon =[
+    public function getPokemon()
+    {
+        $pokemon = [
+            "nombre" => "Bellsprout",
+            "descripcion" => "Si detecta algún movimiento a su alrededor, sea cuando sea, reacciona enseguida extendiendo sus finas lianas en esa dirección",
+            "imagen" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/069.png",
+            "codigo" => "N.º 0069",
+        ];
+        return $this->render("pokemons/pokemon.html.twig", ["pokemon" => $pokemon]);
+    }
+    #[Route("/pokemons")]
+    public function getPokemons()
+    {
+        $pokemons = [
+            [
                 "nombre" => "Bellsprout",
                 "descripcion" => "Si detecta algún movimiento a su alrededor, sea cuando sea, reacciona enseguida extendiendo sus finas lianas en esa dirección",
                 "imagen" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/069.png",
-                "codigo" => "N.º 0069",
-            ];   
-            return $this -> render("pokemons/pokemon.html.twig", ["pokemon" => $pokemon]);
-
+                "codigo" => "N.º 0069"
+            ],
+            [
+                "nombre" => "Pikachu",
+                "descripcion" => "Pika Pikachuuuuuuu",
+                "imagen" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/066.png",
+                "codigo" => "N.º 0066"
+            ],
+            [
+                "nombre" => "BolaPincho",
+                "descripcion" => "Si detecta algún movimiento a su alrededor, sea cuando sea, reacciona enseguida extendiendo sus finas lianas en esa dirección",
+                "imagen" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/077.png",
+                "codigo" => "N.º 0077"
+            ],
+        ];
+        return $this->render("pokemons/pokemons.html.twig", ["pokemons" => $pokemons]);
     }
 };
